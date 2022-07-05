4e799866f7b62a2b0315cb473c8512cfb2dde88f
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-07-05 10:39:26 +0200

    creating cart widget with cart icon

---
+++ src/components/CartWidget.jsx
@@ -0,0 +1,10 @@
+const CartWidget = () => {
+    return (
+        <div className="position-relative">    
+            <i className="bi-cart2" style={{ fontSize: 30 }}></i>
+            <span class="badge rounded-pill badge-notification bg-danger position-absolute">3</span>
+        </div>
+    )
+}
+
+export default CartWidget;
