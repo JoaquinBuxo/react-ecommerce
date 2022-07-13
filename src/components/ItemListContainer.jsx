a4b4c181d12c2e7613258acc85079323d1070a82
Joaquín Buxó <joaquin.buxo@gmail.com>
2022-07-13 17:08:17 +0200

    adding spinner while data loading

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -1,22 +1,33 @@
 import products from "../data/MOCK_DATA.json";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import ItemList from "./ItemList";
 
 const ItemListContainer = (props) => {
   const [productList, setProductList] = useState([]);
+  const [spinner, setSpinner] = useState(false);
 
-  const getProductList = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(products);
-    }, 2000);
-  });
-
-  getProductList.then((res) => setProductList(res));
+  const getProductList = useEffect(() => {
+    setSpinner(true);
+    new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve(products);
+      }, 2000);
+    }).then((res) => {
+      setSpinner(false);
+      setProductList(res);
+    });
+  }, []);
 
   return (
     <div className="container mt-3">
       <h1>{props.title}</h1>
-      <ItemList items={productList} />
+      {spinner ? (
+        <div class="d-flex justify-content-center">
+          <div class="spinner-border" role="status" />
+        </div>
+      ) : (
+        <ItemList items={productList} />
+      )}
     </div>
   );
 };
